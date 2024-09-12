066bf579955e916ebd11da1e83c11618471cef2d
adaliu888 <123860165+adaliu888@users.noreply.github.com>
2024-09-12 10:18:44 +0800

    Update time.go

--- log/time.go
+++ log/time.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-
+	//打印當前的日志
 	fmt.Println(time.Now().Format("2006-01-02 15:04:05"))
-	fmt.Println("CURRENT TIME: ", time.Now().Year(), time.Now().Month(), time.Now().Day)
+	fmt.Println("CURRENT TIME: ", time.Now().Year(), time.Now().Month(), time.Now().Day())
 }
