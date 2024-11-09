7085ee4cb2bff62237585c0acb25d0f23192aa2a
adaliu888 <123860165+adaliu888@users.noreply.github.com>
2024-11-09 14:47:22 +0800

    update

---
+++ gostr/gorune.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	str := "Hello, 世界"
+	runes := []rune(str)
+	for i, r := range runes {
+		fmt.Printf("Position %d: %c\n", i, r)
+	}
+	// Output:
+	// Position 0: H
+	// Position 1: e
+	// Position 2: l
+	// Position 3: l
+	// Position 4: o
+	// Position 5: ,
+	// Position 6: 世
+	// Position 7: 界
+
+	bytes := []byte(str)
+	for i, b := range bytes {
+		fmt.Printf("Position %d: %c\n", i, b)
+	}
+	// Output:
+
+}
